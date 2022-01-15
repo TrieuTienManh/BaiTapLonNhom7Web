de6764364992c3e2b9ae1e0e99eaa660bbfcc098
19-1951060728-DuongThiHong <94340883+19-1951060728-DuongThiHong@users.noreply.github.com>
2022-01-15 21:53:36 +0700

    hoàn thành login

--- login.php
+++ login.php
@@ -1,255 +1,51 @@
 <!DOCTYPE html>
-<html lang="en" >
+<html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <title>Login</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
-  <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700" rel="stylesheet">  
-  <style>
-  	.align {
-  -webkit-box-align: center;
-      -ms-flex-align: center;
-          align-items: center;
-  display: -webkit-box;
-  display: -ms-flexbox;
-  display: flex;
-  -webkit-box-orient: vertical;
-  -webkit-box-direction: normal;
-      -ms-flex-direction: column;
-          flex-direction: column;
-  -webkit-box-pack: center;
-      -ms-flex-pack: center;
-          justify-content: center;
-}
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"
+        integrity="sha512-Fo3rlrZj/k7ujTnHg4CGR2D7kSs0v4LLanw2qksYuRlEzO+tcaEPQogQ0KaoGN26/zrn20ImR1DfuLWnOo7aBA=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
 
-.grid {
-  margin-left: auto;
-  margin-right: auto;
-  max-width: 320px;
-  max-width: 20rem;
-  width: 90%;
-}
-
-.hidden {
-  border: 0;
-  clip: rect(0 0 0 0);
-  height: 1px;
-  margin: -1px;
-  overflow: hidden;
-  padding: 0;
-  position: absolute;
-  width: 1px;
-}
-.icons {
-  display: none;
-}
-
-.icon {
-  display: inline-block;
-  fill: #606468;
-  font-size: 16px;
-  font-size: 1rem;
-  height: 1em;
-  vertical-align: middle;
-  width: 1em;
-}
-
-/* layout/base.css */
-
-* {
-  -webkit-box-sizing: inherit;
-          box-sizing: inherit;
-}
-
-html {
-  -webkit-box-sizing: border-box;
-          box-sizing: border-box;
-  font-size: 100%;
-  height: 100%;
-}
-img{
-  display: inline-block;
-  width: 700px;
-  height: 100%;
-
-  margin-left: 150px;
-
-}
-
-body {
-  background-color: #2c3338;
-  color: #606468;
-  font-family: 'Open Sans', sans-serif;
-  font-size: 14px;
-  font-size: 0.875rem;
-  font-weight: 400;
-  height: 100%;
-  line-height: 1.5;
-  margin: 0;
-  min-height: 100vh;
-}
-
-/* modules/anchor.css */
-
-a {
-  color: #eee;
-  outline: 0;
-  text-decoration: none;
-}
-
-a:focus,
-a:hover {
-  text-decoration: underline;
-}
-
-/* modules/form.css */
-
-input {
-  background-image: none;
-  border: 0;
-  color: inherit;
-  font: inherit;
-  margin: 0;
-  outline: 0;
-  padding: 0;
-  -webkit-transition: background-color 0.3s;
-  transition: background-color 0.3s;
-}
-
-input[type='submit'] {
-  cursor: pointer;
-}
-
-.form {
-  margin: -14px;
-  margin: -0.875rem;
-}
-
-.form input[type='password'],
-.form input[type='email'],
-.form input[type='submit'] {
-  width: 100%;
-}
-
-.form__field {
-  display: -webkit-box;
-  display: -ms-flexbox;
-  display: flex;
-  margin: 14px;
-  margin: 0.875rem;
-}
-
-.form__input {
-  -webkit-box-flex: 1;
-      -ms-flex: 1;
-          flex: 1;
-}
-
-/* modules/login.css */
-
-.login {
-  color: #eee;
-}
-
-.login label,
-.login input[type='email'],
-.login input[type='password'],
-.login input[type='submit'] {
-  border-radius: 0.25rem;
-  padding: 16px;
-  padding: 1rem;
-}
-
-.login label {
-  background-color: #363b41;
-  border-bottom-right-radius: 0;
-  border-top-right-radius: 0;
-  padding-left: 20px;
-  padding-left: 1.25rem;
-  padding-right: 20px;
-  padding-right: 1.25rem;
-}
-
-.login input[type='password'],
-.login input[type='email'] {
-  background-color: #3b4148;
-  border-bottom-left-radius: 0;
-  border-top-left-radius: 0;
-}
-
-.login input[type='password']:focus,
-.login input[type='password']:hover,
-.login input[type='email']:focus,
-.login input[type='email']:hover {
-  background-color: #434a52;
-}
-
-.login input[type='submit'] {
-  background-color: #ea4c88;
-  color: #eee;
-  font-weight: 700;
-  text-transform: uppercase;
-}
-
-.login input[type='submit']:focus,
-.login input[type='submit']:hover {
-  background-color: #d44179;
-}
-
-/* modules/email.css */
-
-p {
-  margin-bottom: 24px;
-  margin-bottom: 1.5rem;
-  margin-top: 24px;
-  margin-top: 1.5rem;
-}
-
-.email--center {
-  text-align: center;
-}
-
-  </style>
 </head>
-<body class="align">
-    <div class="login form">
-      <div class="logo_icon">
-      <img src = "public/images/login.png" class = "w-25"  width:500px height: 100% alt = "logo HKN">
+<body>
+    <div class="container">
+        <H2>Login</H2>
+    <form action= "process_login.php" method="post">
+        <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">Email address</label>
+            <input type="email" name = "txtEmail" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+            <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
         </div>
-      <h3 class = "display-4 fs-1 text-center">LOGIN</h3>
-       <?php 
-                    if(isset($_GET['reply'])){
-                        if($_GET['reply'] == 'successfully'){
-                           echo "<p>Bạn đã kích hoạt tài khoản thành công</p>";
-                        }else { 
-                           echo "<p>Kích hoạt tài khoản thất bại</p>";
-                        }
-                    }
-                ?>
-        <form action="controllers/login.php" method="POST" >        
-            <div class="form__field">
-                <label for="login__username">
-                <svg class="icon">
-                    <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#user"></use>
-                </svg>
-                <span class="hidden">Email</span></label>
-                <input id="login__username" type="email" name="email" class="form__input" placeholder="Email" required>
-            </div>            
-            <div class="form__field">
-                <label for="login__password">
-                <svg class="icon">
-                    <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#lock"></use>
-                </svg>
-                <span class="hidden">Password</span></label>
-                <input id="login__password" type="password" name="password" class="form__input" placeholder="Password" required>
-            </div>            
-            <div class="form__field">
-                <input type="submit" value="Sign In">
-            </div>        
-        </form>    
-    	<p class="email--center">Not a member? <a href="register.php">Sign up now</a></p>
-	</div>
-  <svg xmlns="http://www.w3.org/2000/svg" class="icons"><symbol id="arrow-right" viewBox="0 0 1792 1792"><path d="M1600 960q0 54-37 91l-651 651q-39 37-91 37-51 0-90-37l-75-75q-38-38-38-91t38-91l293-293H245q-52 0-84.5-37.5T128 1024V896q0-53 32.5-90.5T245 768h704L656 474q-38-36-38-90t38-90l75-75q38-38 90-38 53 0 91 38l651 651q37 35 37 90z"/></symbol><symbol id="lock" viewBox="0 0 1792 1792"><path d="M640 768h512V576q0-106-75-181t-181-75-181 75-75 181v192zm832 96v576q0 40-28 68t-68 28H416q-40 0-68-28t-28-68V864q0-40 28-68t68-28h32V576q0-184 132-316t316-132 316 132 132 316v192h32q40 0 68 28t28 68z"/></symbol><symbol id="user" viewBox="0 0 1792 1792"><path d="M1600 1405q0 120-73 189.5t-194 69.5H459q-121 0-194-69.5T192 1405q0-53 3.5-103.5t14-109T236 1084t43-97.5 62-81 85.5-53.5T538 832q9 0 42 21.5t74.5 48 108 48T896 971t133.5-21.5 108-48 74.5-48 42-21.5q61 0 111.5 20t85.5 53.5 62 81 43 97.5 26.5 108.5 14 109 3.5 103.5zm-320-893q0 159-112.5 271.5T896 896 624.5 783.5 512 512t112.5-271.5T896 128t271.5 112.5T1280 512z"/></symbol></svg>
+        <div class="mb-3">
+            <label for="exampleInputPassword1" class="form-label">Password</label>
+            <input type="password" name="txtPassword" class="form-control" id="exampleInputPassword1">
+        </div>
+        <div class="mb-3 form-check">
+            <input type="checkbox" class="form-check-input" id="exampleCheck1">
+            <label class="form-check-label" for="exampleCheck1">Check me out</label>
+            <?php
+                 if(isset($_GET['error'])){
+                     echo "<h5 style = 'color:red'> {$_GET['error']} </h5>";
+                 }
+            ?>
+        </div>
+        <button type="submit" class="btn btn-primary" name="btnSignIn">Sign In</button>
+        </form>
+    </div>
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+        crossorigin="anonymous"></script>
+    <script src="//code.jquery.com/jquery-3.2.0.slim.min.js"></script>
+    <script src="toggle-onoff.js"></script>
 </body>
 </html>
